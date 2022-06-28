b535fd9b5feb54a201aa43f23d3f272bafbd694e
Jimmyforence <jimmyforence@gmail.com>
2022-06-28 15:44:35 -0500

    inicie mi master

---
+++ Assets/js/script.js
@@ -0,0 +1,18 @@
+document.querySelector(".burguer").addEventListener("click", aBurguer);
+
+var line1= document.querySelector(".line1");
+var line2= document.querySelector(".line2");
+var line3= document.querySelector(".line3");
+var hyper= document.querySelector(".login");
+
+
+function aBurguer(){
+    line1.classList.toggle("activeline1");
+    line2.classList.toggle("activeline2");
+    line3.classList.toggle("activeline3");
+    hyper.classList.toggle("logins");
+}
+
+
+
+
