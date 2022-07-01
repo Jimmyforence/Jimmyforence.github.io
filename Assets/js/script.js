30b5d61bba17fd9f4b077a8cbdc3aea2180a0d86
jimmyforence <jimmyforence@gmail.com}>
2022-07-01 01:23:15 -0500

    1.-Hamburguesa corregida., 2.-mejorar la calidad del responsive y margenes, 3.-Se necesita un 780 media

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,15 +4,14 @@ var line1= document.querySelector(".line1");
 var line2= document.querySelector(".line2");
 var line3= document.querySelector(".line3");
 var hyper= document.querySelector(".login");
-
+var side= document.querySelector(".side");
 
 function aBurguer(){
     line1.classList.toggle("activeline1");
     line2.classList.toggle("activeline2");
     line3.classList.toggle("activeline3");
+    
     hyper.classList.toggle("logins");
+    side.classList.toggle("sidekick");
 }
-
-
-
 
