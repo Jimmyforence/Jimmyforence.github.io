25eb899ee30f3f9ded45e09baf59b43fa8830e60
jimmyforence <jimmyforence@gmail.com}>
2022-07-05 01:19:37 -0500

    Added store animation, added content animations, made them responsive

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,3 +15,15 @@ function aBurguer(){
     side.classList.toggle("sidekick");
 }
 
+// $(function () {
+//     var includes = $('[data-include]')
+//     $.each(includes, function () {
+//       var file = 'views/' + $(this).data('include') + '.html'
+//       $(this).load(file)
+//     })
+//   })
+
+
+
+ 
+  
