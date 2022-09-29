8c091356125f344953ee622febcecee9a8231593
Jimmyforence <jimmyforence@gmail.com>
2022-09-29 06:05:37 -0500

    prueba Entrega 2

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,34 +15,36 @@ function aBurguer(){
     side.classList.toggle("sidekick");
 }
 
-// $(function () {
-//     var includes = $('[data-include]')
-//     $.each(includes, function () {
-//       var file = 'views/' + $(this).data('include') + '.html'
-//       $(this).load(file)
-//     })
-//   })
 
+//arrays
+const carrito = [];
+let prodDisp = [
+    playera1 = { Sku: 1001, precio: 800, Talla: "Extra Chica", imagen: "/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-right-front-62de360d06753.png" },
+    playera2 = {Sku: 1002,precio: 800,Talla: "Chica", imagen :"/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-left-front-62de360d06848.png"},
+    playera3 = {Sku: 1003, precio: 700, Talla: "Extra Chica", imagen:"/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-front-2-62de360d06961.png"},
+    chamarra1 = {Sku: 1004,precio: 1800,Talla: "Chica", imagen: "/Assets/Store assets/Chamarra.jpeg"},
+    chamarra2 = {Sku: 1005,precio: 1800,Talla: "Chica", imagen: "/Assets/Store assets/Chamarra.jpeg"},
+    chamarra3 = {Sku: 1006, precio: 2000, Talla: "Extra Grande" , imagen: "/Assets/Store assets/Chamarra.jpeg" },
+    playera4 = {Sku: 1007,precio: 800,Talla: "XChica", imagen:"/Assets/Store assets/No Gameplay No hype/unisex-lightweight-t-shirt-white-front-62de31b8449f8.png"},
+    playera5 = {Sku: 1008,precio: 800,Talla: "Chica", imagen: "/Assets/Store assets/No Gameplay No hype/unisex-lightweight-t-shirt-white-front-62de31b8447a6.png"},
+    playera6 = {Sku: 1009,precio: 800,Talla: "Mediana", imagen:"/Assets/Store assets/No Gameplay No hype/unisex-lightweight-t-shirt-true-red-front-62de31b844262.png"},
+    ]
+    
+//Variables
+let PAIndex = carrito.indexOf('playera1.qty') 
+let PBIndex = carrito.indexOf('playera2.qty')     
+let PCIndex = carrito.indexOf('playera3.qty')   
+let total = 0
+// let contador=1
+// let contador2=1
 
-/*let btn = document.getElementsByClassName('btn');*/
-
-let contador=1
-let contador2=1
-const playera1 = {Sku: 1001, precio: 800, Talla: "Extra Chica" }
-const playera2 = {Sku: 1002,precio: 800,Talla: "Chica" }
-const playera3 = {Sku: 1003, precio: 1100, Talla: "Extra Chica" }
-const playera4 = {Sku: 1004,precio: 1800,Talla: "Chica" }
-const playera5 = {Sku: 1002,precio: 1800,Talla: "Chica" }
-const playera6 = {Sku: 1003, precio: 2000, Talla: "Extra Grande" }
-const playera7 = {Sku: 1004,precio: 1800,Talla: "Chica" }
-
-const prodDisp =[playera1,playera2,playera3,playera4,playera5,playera6, playera7];
  
-document.querySelector(".btn").addEventListener("click", addCarrito, true);
-document.querySelector(".btnB").addEventListener("click", addCarrito2, true);
-document.querySelector(".btnC").addEventListener("click", addCarrito3, true);
-document.querySelector(".lupa").addEventListener("click",busca, true);
+//Funciones 
+
+  // Erase
 document.querySelector(".erase").addEventListener("click",elimina, true);
+
+
 
 function elimina(){
     let goma =0
@@ -50,83 +52,162 @@ function elimina(){
     contador2 = goma
     console.log(`${contador}  Producto(s)  agregado al carrito}`)
 }
-function busca (){
-    let buscad = prompt ("ingrese busqueda por precio")
-    let searchresult =  prodDisp.filter ((producto) => producto.precio > buscad)
+//Playera A
+document.querySelector(".btn").addEventListener("click", addCart1, true);
 
-    console.log(searchresult)
-}
-
-function addCarrito3()
+ function addCart1()
 {
-let playeraC = document.createElement("body.aside.div");
+//-1 is a value that returns if there is none on the array
+if (PAIndex === -1){
+let playeraA = document.createElement("carro");
+playeraA.innerHTML = `<div class="cartAside" >
+<img class= "pAI"src="/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-right-front-62de360d06753.png" alt="Chica">
+<p>Chica</p>
+<textarea class="qtyA" name="" id="qty" cols="10" rows="1"> 1 </textarea>
+<button class= "c+A"> + </button> <button class= "c-A"> - </button>
+</div>`;
+document.querySelector(".side").appendChild(playeraA)
+playera1.qty = 1
+carrito.push(playera1) 
+PAIndex = 1
+}
+else{
+    
+    playera1.qty +=1;
+    document.querySelector(".qtyA").value = playera1.qty
+        }
+}
+//Playera B
+document.querySelector(".btnB").addEventListener("click", addCart2, true);
+
+function addCart2()
+{
+//-1 is a value that returns if there is none on the array
+if (PBIndex === -1){
+let playeraB = document.createElement("carro");
+playeraB.innerHTML = `<div class="cartAside" >
+<img class= "pBI"src="/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-left-front-62de360d06848.png" alt="Chica">
+<p>Chica</p>
+<textarea class="qtyB" name="" id="qty" cols="10" rows="1"> 1 </textarea>
+<button class= "c+C"> + </button> <button class= "c-C"> - </button>
+</div>`;
+document.querySelector(".side").appendChild(playeraB)
+playera2.qty = 1
+carrito.push(playera2) 
+PBIndex = 1
+}
+else{
+   
+   playera2.qty +=1;
+   document.querySelector(".qtyB").value = playera2.qty
+       }
+}
+//Playera C
+ document.querySelector(".btnC").addEventListener("click", addCart3, true);
+
+ function addCart3()
+{
+//-1 is a value that returns if there is none on the array
+if (PCIndex === -1){
+let playeraC = document.createElement("carro");
 playeraC.innerHTML = `<div class="cartAside" >
 <img class= "pCI"src="/Assets/Store assets/Papás/mens-premium-heavyweight-tee-black-front-2-62de360d06961.png" alt="Chica">
 <p>Chica</p>
-<button class= "c+"> + </button> <button class= "C-"> - </button>
+<textarea class="qtyC" name="" id="qty" cols="10" rows="1"> 1 </textarea>
+<button class= "c+C"> + </button> <button class= "c-C"> - </button>
 </div>`;
-document.body.appendChild(playeraC);
-console.log=playeraC
+document.querySelector(".side").appendChild(playeraC)
+playera3.qty = 1
+carrito.push(playera3) 
+PCIndex = 1
 }
-
-
-
-function addCarrito(){
-    let limite = prompt("ingrese la cantidad de playeras que quieras");
-    while ( contador <= limite) {
-        // console.log(`la cantidad en carrito es ${contador}`)
-        // carrito.pop(playera1)
-        
-        
-        
-        console.log(`${contador} Producto(s)  agregado al carrito}`)
-        console.log(`costo del carrito es ${(playera2.precio*(contador2-1) )+(playera1.precio*(contador))}`)
-        console.log (carrito)
-        contador++;
-        carrito.push(playera1)
-        costo.push(playera1.precio);
-    }
+else{
     
-}
-
-
-function addCarrito2(){
-    let limite = prompt("ingrese la cantidad de playeras que quieras");
-    while ( contador2 <= limite) {
-        console.log(`la cantidad en carrito es ${contador2}`)
-        // carrito.pop(playera2)
-        
-        
-        
-        console.log(`${contador2} Producto(s)  agregado al carrito}`)
-        console.log(`costo del carrito es ${(playera2.precio*(contador2) )+(playera1.precio*(contador-1))}`)
-        console.log (carrito)
-        contador2++;
-        carrito.push(playera2)
-        costo.push(playera2.precio);
-    }
-}
-
-
-const carrito = [];
-const costo = []
-
-
-// let total =0
-// carrito.forEach((item) => (total += item.precio));
-// console.log(total)
-
-    /*if ( contador == 0) {
-        contador= contador+1
-       
+    playera3.qty +=1;
+    document.querySelector(".qtyC").value = playera3.qty
         }
-    else
-    {
-        console.log(`la cantidad en carrito es ${contador}`);
-        contador= contador+1
-    }}
-    
-    console.log(`la cantidad en carrito es ${contador}`)
-} */
+}
+
+
+
+
+
+document.querySelector(".calc").addEventListener("click", getTotal, true);
+
+const pagar = ( playera1.precio * playera1.qty)
+console.log (pagar)
+ function getTotal() {
+    if (carrito.length === 0){
+document.querySelector(".qtyC").value
+//document.querySelector(".totalT").style.display = none
+    }
+    else{
+    document.querySelector(".totalT").value = pagar
+ }
+}
+
+
+let busqueda = document.querySelector(".lupa");
+busqueda.addEventListener("click", busca);
+
+function busca ()
+{
+    let monto =document.querySelector(".look").value;
+    let searchresult =  prodDisp.filter ((producto) => producto.precio >= monto)
+console.log(searchresult);
+}
+
+
+
+// // Encarro
+// 
+// let drawCart = document.getElementById ("carro")
+// //let drawCart = document.getElementById ("btn") opcion de prueba
+
+// const addCart = (cosa) =>
+// { const indexCart = carrito.findIndex((elemento)=>{
+//     return elemento.id === prodDisp[cosa].id
  
-  
+// });
+// // -1 is a value that returns if there is none on the array
+// if (indexCart === -1){
+//     const adderir = 10
+//     // adderir = 1;
+//     carrito.push(adderir);
+//     dibujarCarrito();
+// }
+// else{
+//    carrito[indexCart].cantidad +=1;
+//    dibujarCarrito()
+// }
+// };
+
+
+// document.querySelector(".btnB").addEventListener("click", addCart, true);
+
+// document.getElementById("btn").addEventListener("click", addCart, true);
+
+// // it might be that indice = sku. Double check that.
+// const dibujarCarrito=() => 
+// {
+//  drawCart.innerHTML= "";
+// if(carrito.length>  0){
+//     carrito.forEach((prodDisp,sku) =>
+//     {
+//         total =total + prodDisp.precio * prodDisp.cantidad;
+//         const carritoContainer = document.createElement("div");
+//         carritoContainer.className = "miniCart";
+//         carritoContainer.innerHTML = 
+//         `<img class= "carImg" src="${prodDisp.imagen}"/>
+//         <textarea class="productDetails" name="precio" id="look" cols="9" rows="1">${prodDisp.precio}</textarea>
+//         <textarea class="productDetails" name="cantidad" id="look" cols="9" rows="1">${prodDisp.cantidad}</textarea>
+//         <button class= "c+" onClick="removeProduct(${prodDisp.cantidad+1})"> + </button> <button class= "C-"onClick="removeProduct(${prodDisp.cantidad-1})> - </button>`;
+//         carritoContainer.appendChild(carritoContainer);
+//     });
+//     document.getElementById("cash").value = total;
+// }Else
+// carritoContainer.classList.remove("miniCart");
+// }
+
+
+
